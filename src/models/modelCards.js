52cd1ce87e546a548052a95d475d969cf9175f05
Marcos Gabriel <87018862+marcosgabriel-mm@users.noreply.github.com>
2023-05-02 15:11:56 -0300

    Update modelCards.js

--- src/models/modelCards.js
+++ src/models/modelCards.js
@@ -2,13 +2,11 @@ import { Schema, model } from "mongoose";
 
 const DBC_Schema = new Schema ({
     residencia:String,
-    objects:[
-        {
-            uid:String,
-            name:String
-        }
-    ]
-});
+    objects:[{
+        uid:String,
+        nome:String
+    }]
+ });
 
-const DBCM = model('numeralcards', DBC_Schema);
+const DBCM = model('cartoes', DBC_Schema);
 export default DBCM;
